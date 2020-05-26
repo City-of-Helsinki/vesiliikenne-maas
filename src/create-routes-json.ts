5b2e2db2e3c606f53fccd6b7a65c0e9c7bb7bbd7
Riku Honkanen <riku.honkanen@reaktor.com>
2020-05-26 10:08:21 +0300

    Prefer promises over callbacks

--- src/create-routes-json.ts
+++ src/create-routes-json.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import fs from 'fs';
+import { promises as fs } from 'fs';
 import { Route } from './types';
 
 const url = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql';
@@ -33,17 +33,15 @@ const filterByAgencyName = (routes: Route[], name: string) => {
   return routes.filter((route) => route.agency.name === name);
 };
 
-const writeJSON = (routes: any) => {
-  fs.writeFile('./routes.json', routes, (err: any) => {
-    if (err) console.log(err);
-    console.log('File saved');
-  });
+const writeJSON = async (routes: string) => {
+  await fs.writeFile('./routes.json', routes);
+  console.log('File saved');
 };
 
 const main = async () => {
   const routes = await fetchRoutes(url, query);
   const filteredRoutes = filterByAgencyName(routes, 'JT-Line Oy');
-  writeJSON(JSON.stringify(filteredRoutes, null, '  '));
+  await writeJSON(JSON.stringify(filteredRoutes, null, '  '));
 };
 
 main();
