b61ef6ff0bdc8b602dee0e49fa59b945165af1ff
Samuli Kärki <sivuosa@sivuosa.xyz>
2020-05-29 14:53:55 +0300

    Serve visualization and the related api

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
 
+import { getFilteredRoutes } from "./create-routes-json";
+
 dotenv.config()
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World !'))
+app.get('/api/mapbox-token', (_, res) =>
+  res.json(process.env.MAPBOX_ACCESS_KEY)
+)
+
+app.get('/api/routes', async (_, res) => res.json(await getFilteredRoutes()))
+
+app.use(express.static('public'))
 
 app.listen(process.env.APP_PORT, () =>
   console.log(`Example app listening at http://localhost:${process.env.APP_PORT}`)
