a465f25dcf35119e6b1e2a610ea8112f08785b04
Riku Honkanen <riku.honkanen@reaktor.com>
2020-06-05 14:58:24 +0300

    Add readTicketDetails

--- src/ticket-renderer.ts
+++ src/ticket-renderer.ts
@@ -2,9 +2,6 @@ import { uuid } from 'uuidv4'
 import qrcode from 'qrcode'
 import express from 'express'
 
-const acode = uuid()
-console.log(acode)
-
 const dummyTicket = {
   validFrom: '2018-05-26T01:30:00.000Z',
   validTo: '2018-06-26T01:30:00.000Z',
@@ -13,13 +10,17 @@ const dummyTicket = {
   agency: 'JT-Line'
 }
 
+const readTicketDetails = async (_: string) => Promise.resolve(dummyTicket)
+
 export const renderTicket = async (
   req: express.Request,
   res: express.Response
 ): Promise<void> => {
   const { uuid } = req.params
-  const qrCodeContents = await qrcode.toDataURL(uuid)
-  const ticket = dummyTicket
+  const [qrCodeContents, ticket] = await Promise.all([
+    qrcode.toDataURL(uuid),
+    readTicketDetails(uuid)
+  ])
 
   res.send(`<ul>
     <li>Voimassa <span>${ticket.validTo}</span>
