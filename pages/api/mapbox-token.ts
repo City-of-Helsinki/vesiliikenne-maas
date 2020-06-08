c35914a4f520bcf80bf5346e7263d54ddbe5019a
Heikki Rauhala <heikki.rauhala@reaktor.fi>
2020-06-08 16:08:17 +0300

    Add a simple route on the next.js side for the mapbox token

---
+++ pages/api/mapbox-token.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+/**
+ * @swagger
+ *
+ * /api/mapbox-token:
+ *   get:
+ *     summary: MapBox token
+ *     description:  MapBox token for the HSL based ferry route display
+ *     responses:
+ *       '200':
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: string
+ */
+export default (req: NextApiRequest, res: NextApiResponse) =>
+  res.json(process.env.MAPBOX_ACCESS_KEY)
