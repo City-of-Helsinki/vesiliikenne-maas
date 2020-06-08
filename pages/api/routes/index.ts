11fb9315cd2f07fbfe08fa23d45a8edd0b5e2942
Riku Honkanen <riku.honkanen@reaktor.com>
2020-06-08 16:24:26 +0300

    Move routes api endpoint to nextjs and create swagger doc

---
+++ pages/api/routes/index.ts
@@ -0,0 +1,25 @@
+import { getFilteredRoutes } from '../../../src/create-routes-json'
+import { NextApiResponse } from 'next'
+
+/**
+ * @swagger
+ *
+ * /api/routes:
+ *   get:
+ *     summary: JT-Line ferry routes
+ *     description: Returns JT-Line ferry routes and their patterns. Fetched from [digitransit](https://digitransit.fi/en/developers/)
+ *     parameters:
+ *
+ *     responses:
+ *       '200':
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *       '404':
+ *         description: Routes not found
+ */
+export default async (_, res: NextApiResponse) => {
+  res.json(await getFilteredRoutes())
+}
