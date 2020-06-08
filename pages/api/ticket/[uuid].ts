bbb0a31d9e9bd35aa5e80791705e1b22fc1211d4
Heikki Rauhala <heikki.rauhala@reaktor.fi>
2020-06-08 12:42:12 +0300

    Fix types in api/ticket/[uuid]

--- pages/api/ticket/[uuid].ts
+++ pages/api/ticket/[uuid].ts
@@ -1,9 +1,12 @@
 import { renderToString } from 'react-dom/server'
 import TicketContainer from '../../components/TicketContainer'
 import { qrCodeWithTicketDetails } from '../../../src/ticket-renderer'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default async (req, res) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { uuid } = req.query
+  if (typeof uuid !== 'string')
+    throw new Error('Argument uuid is not of type string')
   const [qrCodeContents, ticket] = await qrCodeWithTicketDetails(uuid)
   const html = renderToString(
     TicketContainer({
