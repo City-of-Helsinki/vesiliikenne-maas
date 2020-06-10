91237fd56b2a928e6d35476e8d629ea313ac28c8
Heikki Rauhala <heikki.rauhala@reaktor.fi>
2020-06-10 15:57:46 +0300

    Improve error handling to separate unsupported method, failed parsing and storage errors

--- pages/api/ticket/index.ts
+++ pages/api/ticket/index.ts
@@ -1,21 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { createTicket } from '../../../lib/ticket-service'
 import { toNewTicketEntry } from '../../../lib/utils'
+import { NewTicketEntry } from '../../../lib/types'
 
 export default async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
-  if (req.method === 'POST') {
-    try {
-      const newTicketEntry = toNewTicketEntry(req.body)
+  if (req.method !== 'POST')
+    res.status(405).json({ Error: 'Cannot GET' })
 
-      const uuid = await createTicket(newTicketEntry)
-      res.json({ uuid })
-    } catch (error) {
-      res.status(400).send(error.message)
-    }
-  } else {
-    res.status(404).json({ Error: 'Cannot GET' })
+  let newTicketEntry: NewTicketEntry
+  try {
+    newTicketEntry = toNewTicketEntry(req.body)
+  } catch (error) {
+    return res.status(400).send(error.message)
   }
+  const uuid = await createTicket(newTicketEntry)
+  res.json({ uuid })
 }
