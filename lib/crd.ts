4c9be009b41854a66dd583a33f2f6af8eea50f5f
Heikki Rauhala <heikki.rauhala@reaktor.fi>
2020-06-11 15:51:28 +0300

    Import CrdResponse and Ticket on the same line

--- lib/crd.ts
+++ lib/crd.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import moment from 'moment'
-import { CrdResponse } from './types'
-import { Ticket } from './types'
+import { CrdResponse, Ticket } from './types'
 
 const generateObject = (token: string, unixTime: number, uuid: string) => ({
   token,
