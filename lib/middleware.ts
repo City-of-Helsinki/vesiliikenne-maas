4f7c683fea6be288dcaf5136389f83702da5865b
Riku Honkanen <riku.honkanen@reaktor.com>
2020-06-15 12:31:00 +0300

    Add middleware to parse api key from headers

--- lib/middleware.ts
+++ lib/middleware.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next'
 
 export const getXAPIKEY = (req: NextApiRequest): string => {
   return req.headers['x-api-key']
@@ -8,4 +8,11 @@ export const validateApiKey = (req: NextApiRequest): boolean => {
   const apiKey = getXAPIKEY(req)
 
   return false
+}
+
+export const withApiKeyValidation = (handler): any => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    if (!validateApiKey(req)) return res.status(401).send('Invalid api key')
+    return handler(req, res)
+  }
 }
