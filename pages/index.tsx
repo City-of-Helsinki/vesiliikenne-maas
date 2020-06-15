6924cc34bdf659f5619cba2aad6cdafadee106a3
Riku Honkanen <riku.honkanen@reaktor.com>
2020-06-15 16:26:29 +0300

    Move ticket preview under Dev route; Add validation to NODE_ENV

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { NextPage } from 'next'
 
-const Home: NextPage = ({}) => {
+const Home: NextPage = () => {
   return (
     <div>
       <h3>Index</h3>
@@ -13,12 +13,12 @@ const Home: NextPage = ({}) => {
           <a href="/specs/redoc.html">API docs</a>
         </li>
         <li>
-          <a href="/ticket/c9a5c85d-7d8a-4f8c-8424-ba7f6434d010">
+          <a href="dev/ticket/c9a5c85d-7d8a-4f8c-8424-ba7f6434d010">
             Sample ticket
           </a>
         </li>
         <li>
-          <a href="/ticket/buy">Buy ticket</a>
+          <a href="dev/ticket/buy">Buy ticket</a>
         </li>
       </ul>
     </div>
