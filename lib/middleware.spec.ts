f638fe928d7b4f4ba30a409e5e5c5a85dfdeff45
Riku Honkanen <riku.honkanen@reaktor.com>
2020-06-16 15:47:50 +0300

    Remove repeat from tests

--- lib/middleware.spec.ts
+++ lib/middleware.spec.ts
@@ -3,30 +3,26 @@ import bcrypt from 'bcrypt'
 
 const generateHash = async (string: string) => await bcrypt.hash(string, 10)
 
+const mockRequest = (apikeyContent: string) => ({
+  headers: {
+    'x-api-key': apikeyContent,
+  },
+})
+
 describe('api key authentication', () => {
   it('returns false with wrong token', async () => {
-    const mockRequest = {
-      headers: {
-        'x-api-key': 'wrongsecret',
-      },
-    }
     const hash = await generateHash('secret')
-    const result = await authenticateApiKey(mockRequest, hash)
+    const result = await authenticateApiKey(mockRequest('wrongsecret'), hash)
     expect(result).toBe(false)
   })
 
   it('returns true with right token', async () => {
-    const mockRequest = {
-      headers: {
-        'x-api-key': 'secret',
-      },
-    }
     const hash = await generateHash('secret')
-    const result = await authenticateApiKey(mockRequest, hash)
+    const result = await authenticateApiKey(mockRequest('secret'), hash)
     expect(result).toBe(true)
   })
 
-  it('returns false if header is empty object', async () => {
+  it('returns false if headers is empty object', async () => {
     const mockRequest = {
       headers: {},
     }
@@ -36,23 +32,13 @@ describe('api key authentication', () => {
   })
 
   it('returns false if hash is empty string', async () => {
-    const mockRequest = {
-      headers: {
-        'x-api-key': 'secret',
-      },
-    }
-    const result = await authenticateApiKey(mockRequest, '')
+    const result = await authenticateApiKey(mockRequest('secret'), '')
     expect(result).toBe(false)
   })
 
   it('returns false if header x-api-key is empty string', async () => {
-    const mockRequest = {
-      headers: {
-        'x-api-key': '',
-      },
-    }
     const hash = await generateHash('secret')
-    const result = await authenticateApiKey(mockRequest, hash)
+    const result = await authenticateApiKey(mockRequest(''), hash)
     expect(result).toBe(false)
   })
 })
