baa9ce33f5cebd162d5fc4c46f6cb13829f11602
Riku Honkanen <riku.honkanen@reaktor.com>
2020-06-16 13:11:44 +0300

    Make types better

--- lib/middleware.ts
+++ lib/middleware.ts
@@ -1,20 +1,24 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { isString } from './utils'
 
-export const getXAPIKEY = (req: NextApiRequest): string | false =>
+export const getXAPIKEY = (
+  req: Pick<NextApiRequest, 'headers'>,
+): string | false =>
   isString(req.headers['x-api-key']) && req.headers['x-api-key']
 
-export const authenticateApiKey = (req: NextApiRequest): boolean => {
+export const authenticateApiKey = (
+  req: Pick<NextApiRequest, 'headers'>,
+  token: string,
+): boolean => {
   const apiKey = getXAPIKEY(req)
-  if (apiKey) {
-    // validate
-  }
-  return false
+  return apiKey === token
 }
 
 export const withApiKeyAuthentication = (
   handler: (req: NextApiRequest, res: NextApiResponse) => Promise<void>,
 ) => (req: NextApiRequest, res: NextApiResponse): Promise<void> | void => {
-  if (!authenticateApiKey(req)) return res.status(401).send('Invalid api key')
+  const token = process.env.API_TOKEN || ''
+  if (!authenticateApiKey(req, token))
+    return res.status(401).send('Invalid api key')
   return handler(req, res)
 }
