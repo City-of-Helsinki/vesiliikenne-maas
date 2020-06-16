bfc078a18668e6f08cdb14ff705d968523c11456
Riku Honkanen <riku.honkanen@reaktor.com>
2020-06-16 14:40:39 +0300

    Add bcrypt comparison to api key authentication

--- lib/middleware.ts
+++ lib/middleware.ts
@@ -1,24 +1,26 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { isString } from './utils'
+import bcrypt from 'bcrypt'
+
+const WHIM_API_KEY_HASH = process.env.WHIM_API_KEY_HASH || ''
 
 export const getXAPIKEY = (
   req: Pick<NextApiRequest, 'headers'>,
 ): string | false =>
   isString(req.headers['x-api-key']) && req.headers['x-api-key']
 
-export const authenticateApiKey = (
+export const authenticateApiKey = async (
   req: Pick<NextApiRequest, 'headers'>,
-  token: string,
-): boolean => {
+  hash: string,
+): Promise<boolean> => {
   const apiKey = getXAPIKEY(req)
-  return apiKey === token
+  return apiKey ? await bcrypt.compare(apiKey, hash) : false
 }
 
 export const withApiKeyAuthentication = (
   handler: (req: NextApiRequest, res: NextApiResponse) => Promise<void>,
-) => (req: NextApiRequest, res: NextApiResponse): Promise<void> | void => {
-  const token = process.env.API_TOKEN || ''
-  if (!authenticateApiKey(req, token))
-    return res.status(401).send('Invalid api key')
+) => async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  const validApiKey = await authenticateApiKey(req, WHIM_API_KEY_HASH)
+  if (!validApiKey) return res.status(401).send('Invalid api key')
   return handler(req, res)
 }
