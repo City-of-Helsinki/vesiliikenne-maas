665a0dbcfe5ec94aab65cf8f7ed8e212ab67a3db
Riku Honkanen <riku.honkanen@reaktor.com>
2020-06-18 16:33:56 +0300

    Create api endpoint to show available tickets
    Co-authored-by: Samuli Kärki <sivuosa@users.noreply.github.com>

---
+++ pages/api/maasglobal/tickets.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { promises as fs } from 'fs'
+import { withApiKeyAuthentication } from '../../../lib/middleware'
+
+/**
+ * @swagger
+ *
+ * /api/maasglobal/tickets:
+ *   post:
+ *     summary: Lists tickets available for purchase
+ *     description: Lists tickets provided by JT-Line for purchase via the MaaS app
+ *     responses:
+ *       '200':
+ *         description: Success
+ *       '401':
+ *         description: Invalid api key
+ *       '500':
+ *         description: Server error
+ */
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    res.json(await fs.readFile('./JTline-tickets.json'))
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+export default withApiKeyAuthentication(handler)
