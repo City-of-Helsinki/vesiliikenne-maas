9128f2c8ef9b1537a89fdabbbc24a27d5d1e4960
Pasi Heikkinen <pasi.heikkinen@reaktor.com>
2020-06-23 14:14:03 +0300

    Add a database test

---
+++ lib/db.spec.ts
@@ -0,0 +1,9 @@
+import { pool } from "./db";
+
+describe('Database', () => {
+  it('responds', async () => {
+    expect.assertions(1)
+    const result = await pool.query("select now()")
+    expect(result.rows.length).toEqual(1)
+  })
+})
