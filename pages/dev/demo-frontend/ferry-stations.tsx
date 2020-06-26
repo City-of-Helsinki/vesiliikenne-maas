cf6b2e2e6640f325c5054fbb7f341505a4b003d9
Heikki Rauhala <heikki.rauhala@reaktor.fi>
2020-06-26 13:44:08 +0300

    Initial ticket options list

--- pages/dev/demo-frontend/ferry-stations.tsx
+++ pages/dev/demo-frontend/ferry-stations.tsx
@@ -1,31 +1,59 @@
-import { NextPage, GetServerSideProps } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
 import * as React from 'react'
 import NextError from 'next/error'
 import Link from 'next/link'
+import axios from 'axios'
+import moment from 'moment-timezone'
 
 interface props {
   DEV_API_KEY: string
   NODE_ENV: string
 }
 
+interface TicketOption {
+  amount: number
+  currency: string
+  description: string
+  id: number
+  name: string
+}
+
 
 const FerryStations: NextPage<props> = ({ DEV_API_KEY, NODE_ENV }) => {
-  const [tokenValue, setTokenValue] = React.useState('')
+  const [maasTickets, setMaasTickets] = React.useState<TicketOption[]>([])
 
   if (NODE_ENV !== 'development') {
     return <NextError statusCode={404} />
   }
 
+  React.useEffect(() => {
+    async function getTickets() {
+      const currentTimeMS = moment().tz('Europe/Helsinki').valueOf()
+      const response = await axios.get(
+        `/api/maas/tickets?startTime=${currentTimeMS}`,
+        {
+          headers: {
+            'x-api-key': DEV_API_KEY,
+          },
+        },
+      )
+
+      setMaasTickets(response.data)
+    }
+
+    getTickets()
+  }, [])
+
+  const ticketList = maasTickets.map(ticket => (
+    <li key={ticket.name}>{JSON.stringify(ticket, null, 2)}</li>
+  ))
+
   return (
-      <div>
+    <div>
     <div>JTLine stations on a map </div>
     <div>Ticket options
         <ul>
-            <li>
-              <Link href="ticket-purchase">
-                JTLine Island Hopping ticket, clicking will take to ticket purchase
-              </Link>
-            </li>
+            {ticketList}
         </ul>
     </div>
     </div>
