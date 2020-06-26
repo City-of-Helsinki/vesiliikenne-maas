9b33cb94f5f7849b15da8d97f9a19eb16f51a52f
Riku Honkanen <riku.honkanen@reaktor.com>
2020-06-26 14:14:16 +0300

    Add proptypes

--- pages/dev/demo-frontend/MapComponent.tsx
+++ pages/dev/demo-frontend/MapComponent.tsx
@@ -1,15 +1,18 @@
 import { Map, TileLayer } from 'react-leaflet'
 import * as React from 'react'
+import { NextPage } from 'next'
 
-const MapComponent = ({ accessToken }) => {
+const MapComponent: NextPage<{ accessToken: string }> = ({ accessToken }) => {
   const position = [60.167121, 24.955411]
   return (
-    <Map center={position} zoom={13}>
-      <TileLayer
-        url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=${accessToken}`}
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-    </Map>
+    <div>
+      <Map center={position} zoom={13} style={{ height: '80vh' }}>
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=${accessToken}`}
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </Map>
+    </div>
   )
 }
 
