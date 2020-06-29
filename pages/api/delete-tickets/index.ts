3adcaecbf62f4a9c6ed2efdca708b893d589d57f
Riku Honkanen <riku.honkanen@reaktor.com>
2020-06-29 17:12:42 +0300

    Add endpoint to delete tickets

--- pages/api/delete-tickets/index.ts
+++ pages/api/delete-tickets/index.ts
@@ -3,10 +3,14 @@ import { withApiKeyAuthentication } from '../../../lib/middleware'
 import { unlink } from 'fs/promises'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+<<<<<<< HEAD
   if (process.env.ALLOW_DEMO_FRONTEND !== 'allow') return res.status(401).end()
   if (req.method !== 'POST') {
     return res.status(405).json({ Error: 'Cannot GET' })
   }
+=======
+  if (process.env.ALLOW_DEMO_FRONTEND !== 'allow') res.status(401)
+>>>>>>> 8a2cd7f... Add endpoint to delete tickets
   try {
     await unlink('./tickets.csv')
     res.status(200).send('Tickets deleted')
