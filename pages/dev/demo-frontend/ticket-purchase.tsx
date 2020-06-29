596c1854b4db02036876619ab13fca2b358d77aa
Pasi Heikkinen <pasi.heikkinen@reaktor.com>
2020-06-29 11:18:10 +0300

    Add some styles to the ticket purchasing page

--- pages/dev/demo-frontend/ticket-purchase.tsx
+++ pages/dev/demo-frontend/ticket-purchase.tsx
@@ -4,11 +4,14 @@ import NextError from 'next/error'
 import Router from 'next/router'
 import { TSPTicket } from '../../../lib/types'
 import { formatPrice } from '../../../lib/currency'
+import { CSSProperties } from 'react'
+import { ColorProperty } from 'csstype';
+import { ParsedUrlQuery } from 'querystring'
 
 interface props {
   DEV_API_KEY: string
   NODE_ENV: string,
-  ticket: TSPTicket,
+  ticket: TSPTicket | null,
 }
 
 
@@ -38,9 +41,53 @@ const TicketPurchase: NextPage<props> = ( { DEV_API_KEY, NODE_ENV, ticket }) => 
     await Router.push('/dev/demo-frontend/ferry-stations')
   }
 
+  const o: CSSProperties = {
+    fontFamily: "'Roboto', sans-serif",
+  }
+
+  const accentColor: ColorProperty = "#4c81af"
+
+  const buttonItemStyle: CSSProperties = {
+    display: "inline",
+    //float: "left",
+  }
+
+  const baseButtonStyle: CSSProperties = {
+    border: "none",
+    color: "white",
+    padding: "10px",
+    textAlign: "center",
+    textDecoration: "none",
+    display: "inline-block",
+    fontSize: "1em",
+    margin: "4px 2px",
+    borderRadius: "4px",
+    fontWeight: 600,
+    borderStyle: "solid",
+    borderWidth: "2px",
+    borderColor: accentColor,
+  }
+
+  const secondaryButtonStyle: CSSProperties = {
+    ...baseButtonStyle,
+    color: accentColor,
+    backgroundColor: "transparent",
+    borderStyle: "solid",
+    borderWidth: "2px",
+  }
+
+  const primaryButtonStyle: CSSProperties = {
+    ...baseButtonStyle,
+    backgroundColor: accentColor,
+  }
+
+  if (ticket == undefined) {
+    return (<div>No ticket</div>)
+  }
+
   return (
-    <div>
-      <h1>Confirm ticket</h1>
+    <div style={o}>
+      <h1 style={{fontSize: "1em"}}>Confirm ticket</h1>
       <table>
         <tbody>
         <tr>
@@ -56,17 +103,32 @@ const TicketPurchase: NextPage<props> = ( { DEV_API_KEY, NODE_ENV, ticket }) => 
         </tbody>
       </table>
 
-      <form onSubmitCapture={ () => {handlePurchaseClick().then() }
-      }>
-        <li>
-          <button onClickCapture={ () => { handleCancelClick().then() }}>Cancel</button>
-        </li>
-        <li>
-          <button>Confirm purchase</button>
-        </li>
+      <form onSubmit={ (e) => { e.preventDefault(); handlePurchaseClick().then() }}>
+        <ul style={{listStyleType: "none", paddingLeft: 0}}>
+          <li style={buttonItemStyle}>
+            <button style={secondaryButtonStyle} onClickCapture={ () => { handleCancelClick().then() }}>Cancel</button>
+          </li>
+          <li style={buttonItemStyle}>
+            <button style={primaryButtonStyle}>Confirm purchase</button>
+          </li>
+        </ul>
       </form>
     </div>
   )
+}
+
+const parseTicket = (query: ParsedUrlQuery): TSPTicket | null => {
+  const result = query as unknown as TSPTicket
+  if (result.id === undefined) {
+    return null
+  }
+  if (result.currency === undefined) {
+    return null
+  }
+  if (result.amount === undefined) {
+    return null
+  }
+  return result
 }
 
 export default TicketPurchase
@@ -76,7 +138,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     props: {
       DEV_API_KEY: process.env.DEV_API_KEY,
       NODE_ENV: process.env.NODE_ENV,
-      ticket: context.query as unknown as TSPTicket,
+      ticket: parseTicket(context.query),
     },
   }
 }
