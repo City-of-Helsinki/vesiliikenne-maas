fb16038f8bfb8ade53bcb8fc3fe242cb831331ff
Riku Honkanen <riku.honkanen@reaktor.com>
2020-07-01 16:11:17 +0300

    Add agency and discount_group to ticket_types

--- actions/gtfs-import/create-tickets-migration.sql
+++ actions/gtfs-import/create-tickets-migration.sql
@@ -1,31 +1,33 @@
 SET search_path TO public;
 
 CREATE TABLE IF NOT EXISTS ticket_types(
-  "id" INTEGER NOT NULL,
+  id INT GENERATED ALWAYS AS IDENTITY,
   "description" VARCHAR(255),
   "name" VARCHAR(100) NOT NULL,
   amount MONEY NOT NULL,
+  discount_group VARCHAR(100) NOT NULL,
+  agency VARCHAR(100) NOT NULL,
   PRIMARY KEY (id)
 );
 
 INSERT INTO ticket_types (
-  "id",
   "description",
   "name",
+  discount_group,
+  agency,
   amount
 ) VALUES (
-  1,
   'Hop-on hop-off -style ticket at the islands Lonna, Vallisaari and Suomenlinna.',
   'Island Hopping',
+  'adult',
+  'JT-Line',
   12.00
 );
 
 
 CREATE TABLE IF NOT EXISTS tickets(
   uuid VARCHAR(64) NOT NULL,
-  agency VARCHAR(64) NOT NULL,
   ticket_type_id INTEGER REFERENCES ticket_types(id) NOT NULL,
-  discount_group VARCHAR(64) NOT NULL,
   valid_from TIMESTAMPTZ NOT NULL,
   valid_to TIMESTAMPTZ NOT NULL,
   PRIMARY KEY (uuid)
