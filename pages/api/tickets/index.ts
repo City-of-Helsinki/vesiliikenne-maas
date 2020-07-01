3a7327b8e2f78cd971dee9a9e27ee140adbc15cf
Riku Honkanen <riku.honkanen@reaktor.com>
2020-07-01 16:18:14 +0300

    Change functions work with DB modifications

--- pages/api/tickets/index.ts
+++ pages/api/tickets/index.ts
@@ -51,10 +51,14 @@ import { createJWT } from '../../../lib/utils'
  *         description: Server error
  */
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const tickets = await getTickets()
+  try {
+    const tickets = await getTickets()
 
-  const jwToken = await createJWT({ tickets })
-  res.json(jwToken)
+    const jwToken = await createJWT({ tickets })
+    res.json(jwToken)
+  } catch (error) {
+    res.status(500).send({ error })
+  }
 }
 
 export default withApiKeyAuthentication(handler)
