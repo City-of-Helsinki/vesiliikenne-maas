826924edc55f434bb38e8ca0f4f6d38073521576
Samuli Kärki <github@samuli.dev>
2020-07-10 16:22:22 +0300

    Make the GTFS Import script use the DATABASE_URL variable instead of PG* variables

--- scripts/test_and_import_gtfs.sh
+++ scripts/test_and_import_gtfs.sh
@@ -2,10 +2,25 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+DATABASE_REGEX="^postgres:\/\/(.+):(.+)@(.+):5432\/(.+)$"
+
+# Since Heroku provides automatically only the DATABASE_URL,
+# we need to parse the env variables from it, as they are needed
+# by the gtfs-sql-importer.
+# 
+# We could set them manually, but less manual configuration the better,
+# and as they are only needed for this script, it's better that they are
+# transient.
+export PGUSER=$(sed -r "s/$DATABASE_REGEX/\1/" <<< "$DATABASE_URL")
+export PGPASSWORD=$(sed -r "s/$DATABASE_REGEX/\2/" <<< "$DATABASE_URL")
+export PGHOST=$(sed -r "s/$DATABASE_REGEX/\3/" <<< "$DATABASE_URL")
+export PGDATABASE=$(sed -r "s/$DATABASE_REGEX/\4/" <<< "$DATABASE_URL")
+
 cleanup() {
   echo "Cleaning up created directories"
   cd ..
   rm -rf gtfs-sql-importer gtfs.zip
+  unset PGUSER PGPASSWORD PGHOST PGDATABASE
   exit
 }
 
