ac3dff9bdfab824d22b472f83cfd3fec53637766
Riku Honkanen <riku.honkanen@reaktor.com>
2020-07-16 12:36:00 +0300

    Create generic type validator

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import jsonwebtoken from 'jsonwebtoken'
+import * as t from 'io-ts'
+import { PathReporter } from 'io-ts/lib/PathReporter'
+import { isLeft } from 'fp-ts/lib/Either'
 
 export const isString = (text: any): text is string => {
   return typeof text === 'string' || text instanceof String
@@ -7,7 +10,7 @@ export const isString = (text: any): text is string => {
 
 export const parseString = (string: any, objectKey: string): string => {
   if (!string || !isString(string)) {
-    throw new Error(`Incorrect or missing ${objectKey}: ${string}`)
+    throw new TypeError(`Incorrect or missing ${objectKey}: ${string}`)
   }
 
   return string
@@ -15,13 +18,25 @@ export const parseString = (string: any, objectKey: string): string => {
 
 export const parseNumber = (num: any): number => {
   if (isNaN(num) || !num) {
-    throw new Error(`${num} is not a number`)
+    throw new TypeError(`${num} is not a number`)
   }
   return Number(num)
 }
 
 export const parseLocale = (locale: string | string[]): string =>
   locale ? locale.toString() : 'en'
+
+export const validate = <A, I, O>(
+  runtimeType: t.Type<A, O, I>,
+  inputToValidate: I,
+): A => {
+  const errorOrResult = runtimeType.decode(inputToValidate)
+  if (isLeft(errorOrResult)) {
+    throw new TypeError(PathReporter.report(errorOrResult).toString())
+  } else {
+    return errorOrResult.right
+  }
+}
 
 const base64decode = (input: string): string =>
   Buffer.from(input, 'base64').toString('utf-8')
